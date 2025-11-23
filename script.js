18e73a5812e3b90a1ace39ba7714c3b6023801e9
Cyprien Piron <cyprien.piron@gmail.com>
2025-11-23 23:52:16 +0100

    mobile optimized

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ window.addEventListener('scroll', () => {
 // Intersection Observer for fade-in animations
 const observerOptions = {
     threshold: 0.1,
-    rootMargin: '0px 0px -50px 0px'
+    rootMargin: '0px 0px 100px 0px'
 };
 
 const observer = new IntersectionObserver((entries) => {
